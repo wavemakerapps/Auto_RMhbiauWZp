3d5bb867cb84903df2eed3b20b80ba105abcb8c5
root <root@2eb49d31ca4f>
2017-06-22 09:08:26 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_RMhbiauWZp",
+  "homePage" : "Main",
+  "name" : "Auto_RMhbiauWZp",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
